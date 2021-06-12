b2817430c7c503635e2454ec6bdc9b6dbc36bc25
Chris <cmhanyok@gmail.com>
2021-06-11 23:48:55 -0600

    Colliders added
    Colliders for traps and enemy added, triggers when running into each of them

---
+++ Assets/Scripts/PlayerCollisionHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionHandler : MonoBehaviour
+{
+    int trapsLayer;
+    int enemyLayer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        trapsLayer = LayerMask.NameToLayer("Traps");
+        enemyLayer = LayerMask.NameToLayer("Enemy");
+
+    
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == trapsLayer)
+        {
+            Debug.Log("You just ran into Traps!");
+        }
+        if(collision.gameObject.layer == enemyLayer)
+        {
+            Debug.Log("You just ran into Enemy!");
+        }
+    }
+}
